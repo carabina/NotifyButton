b63b494c544ba54bd9c62014b08e15dc1b90863f
Tony M Joseph <Tony@qbadmins-iMac-2.local>
2016-12-08 14:02:28 +0530

    New version

--- Example/NotifyButton/ViewController.swift
+++ Example/NotifyButton/ViewController.swift
@@ -7,17 +7,25 @@
 //
 
 import UIKit
+import NotifyButton
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,NotifyButtonDelegate {
 
+    @IBOutlet weak var notifyMeButton: NotifyButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        notifyMeButton.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func didFinishTask(sender: String, button: NotifyButton) {
+        // do task using the email
+        button.success()  // if success
     }
 
 }
--- NotifyButton.podspec
+++ NotifyButton.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'NotifyButton'
-  s.version          = '0.1.0'
+  s.version          = '0.1.1'
   s.summary          = 'A Subclass on UIButton to notify a user via email.'
 
 # This description is used to generate tags and improve search results.
